2da3d797c26227d26db52b9b4d1ab499438e8e0b
Robert Tarnas <robert.tarnas@protonmail.com>
2024-02-25 10:22:54 +0100

    Initial commit

---
+++ RandomWord.java
@@ -0,0 +1,34 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+
+        int index = 0;
+        String champion = "";
+
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            boolean isChampion = StdRandom.bernoulli(1 / (index + 1.0));
+
+            if (isChampion) {
+                champion = word;
+            }
+            index++;
+        }
+
+        if (index == 0) {
+            StdOut.println("No input provided.");
+        }
+        else {
+            StdOut.println(champion);
+        }
+    }
+}
